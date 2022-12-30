965026ff68ae743bd5000dabbc135b05aef33502
Rodrigo Iriarte <iriartezam18@gmail.com>
2022-12-30 10:46:19 -0400

    Changing Classes from Spanish to English

---
+++ CD-Store/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Store.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        public int categoryId { get; set; }
+        public string name { get; set; }
+        public double unitPrice { get; set; }
+        public DateTime registerDate { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public int status { get; set; }
+        public Product(int productId, int categoryId, string name, double unitPrice, DateTime registerDate, DateTime lastUpdate, int status)
+        {
+            this.productId = productId;
+            this.categoryId = categoryId;
+            this.name = name;
+            this.unitPrice = unitPrice;
+            this.registerDate = registerDate;
+            this.lastUpdate = lastUpdate;
+            this.status = status;
+        }
+    }
+}
