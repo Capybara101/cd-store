04ad84d31f4148b6bb9ed56a4899ce2d14b98ddd
Rodrigo Iriarte <iriartezam18@gmail.com>
2022-12-30 09:21:59 -0400

    Adding SQLite Dependancy

---
+++ CD-Store/DetalleVenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Store
+{
+    internal class DetalleVenta
+    {
+        public int idDetalleVenta { get; set; }
+        public int idVenta { get; set; }
+        public int idProducto { get; set; }
+        public double precioUnitario { get; set; }
+        public int cantidad { get; set; }
+        public DateTime fechaRegistro { get; set; }
+        public DateTime fechaActualizacion { get; set; }
+        public int estado { get; set; }
+    }
+}
---
+++ CD-Store/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Store.Models
+{
+    internal class Categoria
+    {
+        public int idCategoria { get; set; }
+        public string nombre { get; set; }
+        public DateTime fechaRegistro { get; set; }
+        public DateTime fechaActualizacion { get; set; }
+        public int estado { get; set; }
+    }
+}
